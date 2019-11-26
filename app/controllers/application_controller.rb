2d1fe9282400ffdf8fba977d6881a0172603a7a0
cep104 <candicelizabethpeters@gmail.com>
2019-11-26 11:38:33 -0600

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,15 +10,17 @@ class App < Sinatra::Base
         
     end
 
-    post '/teams' do 
-       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-       members = params[:team][:members]
-        
-       members.each do |member_data|
-        SuperHero.new({name: member_data[:name], power: member_data[:power], bio: member_data[:bio]})
-       end
-       @super_heroes = SuperHero.all
+    post '/teams' do
+        @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+        members = params[:team][:members]
+  
+        members.each do |member_params|
+          SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+        end
+        @super_heroes = SuperHero.all
+  
         erb :team
-    end
+      
+ 	end
 
 end
