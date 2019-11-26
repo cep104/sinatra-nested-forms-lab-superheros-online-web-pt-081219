d4ed623533c074cc58b748b7213e5b696fae5698
cep104 <candicelizabethpeters@gmail.com>
2019-11-26 11:02:57 -0600

    almost done
    one issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+        
+    erb :index 
+        
+    end
+
+    post '/teams' do 
+       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+       members = params[:team][:members]
+        
+       members.each do |member_data|
+        SuperHero.new({name: member_data[:name], power: member_data[:power], bio: member_data[:bio]})
+       end
+       @super_heroes = SuperHero.all
+        erb :team
+    end
 
 end
